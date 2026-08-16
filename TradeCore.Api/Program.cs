1dc3b1c33e73b389b7af18087f0a7f0d3c166c5c
aseelmujahed <aseelmujahed2@gmail.com>
2026-08-16 22:36:55 +0300

    feat: add ASP.NET Core Web API

---
+++ TradeCore.Api/Program.cs
@@ -0,0 +1,24 @@
+using TradeCore.Console.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddSingleton<AccountService>();
+builder.Services.AddSingleton<StockService>();
+builder.Services.AddSingleton<OrderService>();
+builder.Services.AddSingleton<PortfolioService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "TradeCore API v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
